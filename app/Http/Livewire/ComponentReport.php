04e33925abe50d1b543b1547d7feced1ae85f3f1
n1m0d0 <33577927+n1m0d0@users.noreply.github.com>
2023-03-24 12:00:41 -0400

    filtros en reportes por sector y accion

--- app/Http/Livewire/ComponentReport.php
+++ app/Http/Livewire/ComponentReport.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Planning;
+use App\Models\Sector;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -12,14 +13,49 @@ class ComponentReport extends Component
 
     public $search;
 
+    public $sector_id;
+
+    public $sectors;
+
+    public function mount()
+    {
+        $this->search = null;
+        $this->sector_id = null;
+        $this->sectors = Sector::all();
+    }
+
     public function render()
     {
         $Query = Planning::query();
+
+        if ($this->sector_id != null)
+        {
+            $this->updatingSearch();
+            $Query = $Query->where('sector_id', $this->sector_id);
+        }
+
         if ($this->search != null) {
             $this->updatingSearch();
-            $Query = $Query->where('name', 'like', '%' . $this->search . '%');
+            $Query = $Query->where('code', 'like', '%' . $this->search . '%')->orWhere('result_description', 'like', '%' . $this->search . '%')->orWhere('action_description', 'like', '%' . $this->search . '%');
         }
+
         $plannings = $Query->orderBy('id', 'DESC')->paginate(7);
         return view('livewire.component-report', compact('plannings'));
+    }
+
+    public function clear()
+    {
+        $this->reset(['sector_id']);
+    }
+
+    public function resetSearch()
+    {
+        $this->reset(['search']);
+        $this->updatingSearch();
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
     }
 }
