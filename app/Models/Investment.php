4f53958392c7b1ec26694e7e1e821faf001dfb35
n1m0d0 <33577927+n1m0d0@users.noreply.github.com>
2023-04-10 21:48:58 -0400

    implementar operacion de registro de auditoria en los modelos

--- app/Models/Investment.php
+++ app/Models/Investment.php
@@ -5,12 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Investment extends Model
+class Investment extends Model implements Auditable
 {
     use HasFactory;
 
     use SoftDeletes;
+
+    use \OwenIt\Auditing\Auditable;
 
     public function finance()
     {
